30a8175c21cf3030ab7f23fbddfc1b3eae0b2d2f
11Murli11 <murli@cipio.ai>
2024-09-22 02:32:07 +0530

    Hackathon 2024

--- ManageInstances/APItoGriffith.py
+++ ManageInstances/APItoGriffith.py
@@ -258,7 +258,7 @@ def bot_interaction():
 
 # GitHub API URL and PAT
 org_name = "GriffithGithubOrg"
-github_pat = "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"
+github_pat = "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"
 
 # Route to get organization members
 @app.route('/org-members', methods=['GET'])
